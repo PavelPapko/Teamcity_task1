18878786814479f6e79f12da779d9d53f2ff5aff
PapkoPA <trait95@yandex.ru>
2018-05-14 12:28:15 +0300

    commit_7

--- protractor.conf.js
+++ protractor.conf.js
@@ -4,12 +4,25 @@ exports.config = {
     suites: {
         search: ['./Specs/search.spec.js', './Specs/navDrawer.spec.js', './Specs/docs.spec.js']
     },
-
+/*
     onPrepare: function() {
         var AllureReporter = require('jasmine-allure-reporter');
         jasmine.getEnv().addReporter(new AllureReporter({
             resultsDir: 'allure-results'
         }));
+    },
+*/
+    onPrepare: function () {
+        var AllureReporter = require('jasmine-allure-reporter');
+        jasmine.getEnv().addReporter(new AllureReporter());
+        jasmine.getEnv().afterEach(function(done){
+            browser.takeScreenshot().then(function (png) {
+                allure.createAttachment('Screenshot', function () {
+                    return new Buffer(png, 'base64')
+                }, 'image/png')();
+                done();
+            })
+        });
     },
 
     SELENIUM_PROMISE_MANAGER: false,
